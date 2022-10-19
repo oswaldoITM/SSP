dccd4b55ec9764efdc4dcc6c447fa01b30865621
Oswal-DRO <16120188@morelia.tecnm.mx>
2022-10-18 21:07:32 -0500

    Se completo el login, faltan detalles de seguridad de sesiones

---
+++ validar.php
@@ -0,0 +1,43 @@
+<?php
+$usuario=$_POST['usuario'];
+$contraseña=$_POST['contraseña'];
+session_start();
+$_SESSION['usuario']=$usuario;
+
+$conexion=mysqli_connect("localhost","root","","rol");
+
+$consulta="SELECT*FROM usuarios where usuario='$usuario' and contraseña='$contraseña'";
+$resultado=mysqli_query($conexion,$consulta);
+
+$filas=mysqli_fetch_array($resultado);
+
+if (is_array($filas)){
+
+
+    if($filas['id_cargo']==1){ //administrador
+    
+        header("location:admin.php");
+        
+    
+    }else
+    
+    if($filas['id_cargo']==2){ //usuario
+    header("location:usuario.php");
+    }
+
+}else{
+    ?>
+    <?php
+    include("index.html");
+    ?>
+    <h1 class="bad">ERROR EN LA AUTENTIFICACION</h1>
+    <?php
+
+}
+
+
+
+
+
+mysqli_free_result($resultado);
+mysqli_close($conexion);
